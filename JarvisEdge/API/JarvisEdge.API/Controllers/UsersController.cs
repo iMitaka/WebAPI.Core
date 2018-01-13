1f8d300b3adaffe3257622faa3040f3ceda2a47b
imitaka <dimitar.georgiev1992@gmail.com>
2018-01-13 15:36:34 +0200

    refactored code style removed ValuesController.cs added UsersController.cs

---
+++ JarvisEdge/API/JarvisEdge.API/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿namespace JarvisEdge.API.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return this.Ok(new string[] { "value1", "value2" });
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return this.Ok(id);
+        }
+    }
+}
